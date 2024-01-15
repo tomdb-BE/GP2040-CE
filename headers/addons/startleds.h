533fdc7a4d993c4b44e285fa26a592ee04c482c5
Tom De Backer <tomdebacker1@gmail.com>
2024-01-15 18:02:34 +0100

    Test startled upstream mnerge

---
+++ headers/addons/startleds.h
@@ -0,0 +1,227 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2022 Tom De Backer
+ */
+
+#ifndef _STARTLEDS_H_
+#define _STARTLEDS_H_
+
+#include "BoardConfig.h"
+#include <stdint.h>
+#include "AnimationStation.hpp"
+#include "gpaddon.h"
+#include "helper.h"
+#include "GamepadState.h"
+
+#ifndef STARTLEDS_ENABLED
+#define STARTLEDS_ENABLED 0
+#endif
+#ifndef STARTLEDS_COUNT
+#define STARTLEDS_COUNT 4
+#endif
+#ifndef STARTLEDS_PWM_MAXLEVEL
+#define STARTLEDS_PWM_MAXLEVEL 0xFFFF
+#endif
+#ifndef START1_BUTTON_MASK
+#define START1_BUTTON_MASK GAMEPAD_MASK_S2
+#endif
+#ifndef COIN1_BUTTON_MASK
+#define COIN1_BUTTON_MASK GAMEPAD_MASK_S1
+#endif
+#ifndef START2_BUTTON_MASK
+#define START2_BUTTON_MASK 0
+#endif
+#ifndef COIN2_BUTTON_MASK
+#define COIN2_BUTTON_MASK 0
+#endif
+#ifndef START3_BUTTON_MASK
+#define START3_BUTTON_MASK 0
+#endif
+#ifndef COIN3_BUTTON_MASK
+#define COIN3_BUTTON_MASK 0
+#endif
+#ifndef START4_BUTTON_MASK
+#define START4_BUTTON_MASK 0
+#endif
+#ifndef COIN4_BUTTON_MASK
+#define COIN4_BUTTON_MASK 0
+#endif
+#ifndef START_BUTTON_MASKS
+#define START_BUTTON_MASKS START1_BUTTON_MASK | START2_BUTTON_MASK | START3_BUTTON_MASK | START4_BUTTON_MASK
+#endif
+#ifndef COIN_BUTTON_MASKS
+#define COIN_BUTTON_MASKS COIN1_BUTTON_MASK | COIN2_BUTTON_MASK | COIN3_BUTTON_MASK | COIN4_BUTTON_MASK
+#endif
+#ifndef STARTLEDS_START_PIN1
+#define STARTLEDS_START_PIN1 -1
+#endif
+#ifndef STARTLEDS_COIN_PIN1
+#define STARTLEDS_COIN_PIN1 -1
+#endif
+#ifndef STARTLEDS_START_PIN2
+#define STARTLEDS_START_PIN2 -1
+#endif
+#ifndef STARTLEDS_COIN_PIN2
+#define STARTLEDS_COIN_PIN2 -1
+#endif
+#ifndef STARTLEDS_START_PIN3
+#define STARTLEDS_START_PIN3 -1
+#endif
+#ifndef STARTLEDS_COIN_PIN3
+#define STARTLEDS_COIN_PIN3 -1
+#endif
+#ifndef STARTLEDS_START_PIN4
+#define STARTLEDS_START_PIN4 -1
+#endif
+#ifndef STARTLEDS_COIN_PIN4
+#define STARTLEDS_COIN_PIN4 -1
+#endif
+#ifndef STARTLEDS_MARQUEE_PIN
+#define STARTLEDS_MARQUEE_PIN -1
+#endif
+#ifndef STARTLEDS_EXT_START_PIN
+#define STARTLEDS_EXT_START_PIN -1
+#endif
+#ifndef STARTLEDS_EXT_COIN_PIN
+#define STARTLEDS_EXT_COIN_PIN -1
+#endif
+#ifndef STARTLEDS_EXT_START_MASK
+#define STARTLEDS_EXT_START_MASK GAMEPAD_MASK_A1
+#endif
+#ifndef STARTLEDS_EXT_COIN_MASK
+#define STARTLEDS_EXT_COIN_MASK GAMEPAD_MASK_A2
+#endif
+#ifndef STARTLEDS_EXT_MASKS
+#define STARTLEDS_EXT_MASKS STARTLEDS_EXT_START_MASK | STARTLEDS_EXT_COIN_MASK
+#endif
+#ifndef STARTLEDS_START_BRIGHTNESS
+#define STARTLEDS_START_BRIGHTNESS 50
+#endif
+#ifndef STARTLEDS_COIN_BRIGHTNESS
+#define STARTLEDS_COIN_BRIGHTNESS 50
+#endif
+#ifndef STARTLEDS_MARQUEE_BRIGHTNESS
+#define STARTLEDS_MARQUEE_BRIGHTNESS 50
+#endif
+#ifndef STARTLEDS_BRIGHTNESS_STEP
+#define STARTLEDS_BRIGHTNESS_STEP 5
+#endif
+#ifndef STARTLEDS_DEBOUNCE_MILLIS
+#define STARTLEDS_DEBOUNCE_MILLIS 50
+#endif
+
+static const uint8_t STARTLEDS_STATE_LED1 = (1 << 0);
+static const uint8_t STARTLEDS_STATE_LED2 = (1 << 1);
+static const uint8_t STARTLEDS_STATE_LED3 = (1 << 2);
+static const uint8_t STARTLEDS_STATE_LED4 = (1 << 3);
+static const uint8_t STARTLEDS_STATE_ALL_ON  = 0xFF;
+static const uint8_t STARTLEDS_STATE_ALL_OFF = 0;
+
+typedef enum : uint8_t
+{
+	STARTLEDS_ANIM_NONE = 255,
+	STARTLEDS_ANIM_OFF = 0,
+	STARTLEDS_ANIM_SOLID = 1,
+	STARTLEDS_ANIM_BLINK = 2,
+	STARTLEDS_ANIM_CYCLE = 3,
+	STARTLEDS_ANIM_FADE = 4,
+} StartLedsAnimationType;
+
+typedef enum : uint16_t
+{
+	STARTLEDS_SPEED_OFF       = 0,
+	STARTLEDS_SPEED_LUDICROUS = 20,
+	STARTLEDS_SPEED_FASTER    = 100,
+	STARTLEDS_SPEED_FAST      = 250,
+	STARTLEDS_SPEED_NORMAL    = 500,
+	STARTLEDS_SPEED_SLOW      = 1000,
+} StartLedsAnimationSpeed;
+
+struct StartLedsAnimation
+{
+	uint8_t currentState = STARTLEDS_STATE_ALL_OFF;		
+	uint8_t stateMask = 0xFF;
+	StartLedsAnimationType currentType = STARTLEDS_ANIM_NONE;
+	StartLedsAnimationType previousType = STARTLEDS_ANIM_NONE;
+	StartLedsAnimationSpeed speed = STARTLEDS_SPEED_OFF;
+};
+
+static const StartLedsAnimation STARTLEDS_ALL_OFF {
+	currentState: STARTLEDS_STATE_ALL_OFF,
+	stateMask: 0,
+	currentType: STARTLEDS_ANIM_OFF
+};
+
+static const StartLedsAnimation STARTLEDS_ALL_ON {
+	currentState: STARTLEDS_STATE_ALL_ON,
+	currentType: STARTLEDS_ANIM_SOLID
+};
+
+static const StartLedsAnimation STARTLEDS_BLINK_FAST_ALL {
+	currentState: STARTLEDS_STATE_ALL_OFF,
+	currentType: STARTLEDS_ANIM_BLINK,
+	speed: STARTLEDS_SPEED_FAST
+};
+
+static const StartLedsAnimation STARTLEDS_FADE_ALL {
+	currentState: STARTLEDS_STATE_ALL_ON,
+	currentType: STARTLEDS_ANIM_FADE,
+	speed: STARTLEDS_SPEED_LUDICROUS
+};
+
+
+#define StartLedsAddonName "STARTLEDSADDON"
+
+class StartLeds
+{
+public:	
+	std::vector<uint> initialize(std::vector<uint> slices, uint8_t * pins, uint8_t mBrightness, StartLedsAnimation initAnimation);
+	void display();
+	void update() {this->nextAnimationTime = make_timeout_time_ms(0);}
+	void setAnimation(StartLedsAnimation newAnimation) { this->animation = newAnimation; this->update(); }
+	void setType(StartLedsAnimationType newType) { this->animation.currentType = newType; }
+	void setSpeed(StartLedsAnimationSpeed newSpeed) { this->animation.speed = newSpeed; this->update(); }
+	void setMask(uint8_t newMask) { this->animation.stateMask = newMask; this->update(); }
+	void brightnessUp(uint8_t amount = STARTLEDS_BRIGHTNESS_STEP) { this->brightness = this->handleBrightness(amount, true); }
+	void brightnessDown(uint8_t amount = STARTLEDS_BRIGHTNESS_STEP) { this->brightness = this->handleBrightness(amount); }
+	StartLedsAnimation getAnimation() { return this->animation; }
+	StartLedsAnimationSpeed getSpeed() { return this->animation.speed; }
+	StartLedsAnimationType getType() { return this->animation.currentType; }
+	uint8_t getMask() { return this->animation.stateMask; }
+	bool isReady() {return this->ready;}
+private:
+	inline void animate();
+	inline void reset();
+	uint8_t handleBrightness(uint8_t amount, bool negative = false);	
+	absolute_time_t nextAnimationTime = make_timeout_time_ms(0);
+	uint16_t ledLevels[STARTLEDS_COUNT];
+	uint8_t ledPins[STARTLEDS_COUNT];
+	uint8_t brightness;
+	uint8_t maxBrightness;
+	StartLedsAnimation animation;
+	bool fadeIn;
+	bool ready;
+};
+
+class StartLedsAddon : public GPAddon
+{
+public:
+	virtual bool available();
+	virtual void setup();
+	virtual void preprocess() {}
+	virtual void process();
+	virtual std::string name() { return StartLedsAddonName; }	
+private:	
+	StartLeds ledsStart;
+	StartLeds ledsCoin;
+	StartLeds ledsMarquee;
+	bool debounce(uint32_t * ptrDebounceTime);
+	uint8_t creditCount = 0;
+	uint16_t lastButtonsPressed;	
+	uint32_t debounceMarqueeBrightness;
+	uint8_t externalStartPin = 0xFF;
+	uint8_t externalCoinPin	= 0xFF;
+	bool ready;
+};
+
+#endif
---
+++ src/addons/startleds.cpp
@@ -0,0 +1,252 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2022 Tom De Backer
+ */
+
+// Pico Includes
+#include <vector>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "GamepadEnums.h"
+#include "storagemanager.h"
+
+// GP2040 Includes
+#include "addons/startleds.h"
+#include "helper.h"
+
+
+std::vector<uint> StartLeds::initialize(std::vector<uint> slices, uint8_t * pins, uint8_t mBrightness, StartLedsAnimation initAnimation) 
+{
+	if (pins[0] == 0xFF)
+		return slices;
+		
+	uint8_t newPin;
+	this->brightness = mBrightness;
+	this->maxBrightness = mBrightness;
+	for (uint8_t i=0; i < STARTLEDS_COUNT; i++)
+	{
+		newPin = pins[i];
+		this->ledPins[i] = newPin;
+		if (newPin != 0xFF)
+		{
+			gpio_set_function(newPin, GPIO_FUNC_PWM);
+			uint sliceNum = pwm_gpio_to_slice_num(newPin);
+			uint channelNum = pwm_gpio_to_channel(newPin);
+			slices.push_back(sliceNum);
+			pwm_set_chan_level(sliceNum, channelNum, STARTLEDS_PWM_MAXLEVEL);			
+		}
+	}
+	this->animation = initAnimation;
+	this->animate();
+	this->ready = true;
+	return slices;
+}
+
+void StartLeds::display()
+{
+	if (! this->ready)
+		return;
+
+	this->animate();
+	for (int i = 0; i < STARTLEDS_COUNT; i++)
+		if (this->ledPins[i] != 0xFF)
+			pwm_set_gpio_level(this->ledPins[i], this->ledLevels[i]);
+}
+
+inline void StartLeds::animate()
+{
+	uint8_t newState = this->animation.currentState;
+	uint8_t mask = this->animation.stateMask;
+	if (this->animation.currentType != this->animation.previousType)	
+		this->reset();			
+	else if (!time_reached(this->nextAnimationTime))	
+		return;			
+
+	switch (this->animation.currentType)
+	{
+		case STARTLEDS_ANIM_OFF:
+			newState = STARTLEDS_STATE_ALL_OFF | mask;
+			break;
+
+		case STARTLEDS_ANIM_SOLID:
+			newState = STARTLEDS_STATE_ALL_ON & mask;
+			break;	
+
+		case STARTLEDS_ANIM_BLINK:
+			newState = ~newState & mask;
+			this->nextAnimationTime = make_timeout_time_ms(this->animation.speed);
+			break;
+
+		case STARTLEDS_ANIM_FADE:
+			newState = newState & mask;
+			this->brightness = this->handleBrightness(STARTLEDS_BRIGHTNESS_STEP, this->fadeIn);
+			this->nextAnimationTime = make_timeout_time_ms(this->animation.speed);
+			break;
+
+		default:
+			break;
+	}
+
+	this->animation.currentState = newState;
+
+	for (uint8_t i = 0; i < STARTLEDS_COUNT; i++)
+		if (this->ledPins[i] != 0xFF)
+			this->ledLevels[i] = (newState & (1 << i)) ? this->brightness * this->brightness : 0;			
+}
+
+inline void StartLeds::reset()
+{	
+	this->animation.previousType = this->animation.currentType;
+	this->nextAnimationTime = make_timeout_time_ms(0);
+	this->brightness = this->maxBrightness;
+	this->fadeIn = false;
+}
+
+uint8_t StartLeds::handleBrightness(uint8_t amount, bool increase)
+{
+	int16_t nBrightness = this->brightness;
+	uint16_t mBrightness = this->maxBrightness;
+
+	if (increase)	
+	{				
+		nBrightness += 5;
+		if (nBrightness >= mBrightness) {
+			this->fadeIn = false;
+			return mBrightness;
+		}
+		return nBrightness;
+	}
+	
+	nBrightness -= 5;
+	if (nBrightness <= 0) {
+		this->fadeIn = true;
+		return 0;
+	}
+	return nBrightness;
+}
+
+bool StartLedsAddon::available() {
+	AddonOptions options = Storage::getInstance().getAddonOptions();	
+	return options.startLedsAddonEnabled;
+}
+
+void StartLedsAddon::setup() {	
+	AddonOptions options = Storage::getInstance().getAddonOptions();
+	uint8_t startPins[] = {options.startLedsStartPin1, options.startLedsStartPin2, options.startLedsStartPin3, options.startLedsStartPin4};
+	uint8_t coinPins[] = {options.startLedsCoinPin1, options.startLedsCoinPin2, options.startLedsCoinPin3, options.startLedsCoinPin4};
+	uint8_t marqueePins[] = {options.startLedsMarqueePin, 0xFF, 0xFF, 0xFF};
+	uint8_t extStartPin = options.startLedsExtStartPin;
+	uint8_t extCoinPin = options.startLedsExtCoinPin;
+	uint8_t startBrightness = (float)options.startLedsStartBrightness / 100 * 255;	
+	uint8_t coinBrightness = (float)options.startLedsCoinBrightness / 100 * 255;	
+	uint8_t marqueeBrightness = (float)options.startLedsMarqueeBrightness / 100 * 255;
+
+	pwm_config config = pwm_get_default_config();
+	pwm_config_set_clkdiv(&config, 4.f);
+
+	std::vector<uint> sliceNums;
+
+	sliceNums = this->ledsStart.initialize(sliceNums, startPins, startBrightness, STARTLEDS_ALL_OFF); 
+	sliceNums = this->ledsCoin.initialize(sliceNums, coinPins, coinBrightness, STARTLEDS_ALL_ON);
+	sliceNums = this->ledsMarquee.initialize(sliceNums, marqueePins, marqueeBrightness, STARTLEDS_ALL_ON);
+
+	for (auto sliceNum : sliceNums)
+		pwm_set_enabled(sliceNum, true);
+	
+	if (extStartPin != 0xFF)
+	{
+		gpio_init(extStartPin);
+    	gpio_set_dir(extStartPin, GPIO_OUT);
+		gpio_put(extStartPin, 1);
+		this->externalStartPin = extStartPin;
+	}
+
+	if (extCoinPin != 0xFF)
+	{
+		gpio_init(extCoinPin);
+    	gpio_set_dir(extCoinPin, GPIO_OUT);
+		gpio_put(extCoinPin, 1);
+		this->externalCoinPin = extCoinPin;
+	}
+	this->ready = true;
+}
+
+void StartLedsAddon::process()
+{
+    Gamepad * gamepad = Storage::getInstance().GetProcessedGamepad();	
+    
+	uint16_t buttonsPressed = gamepad->state.buttons & (START_BUTTON_MASKS | COIN_BUTTON_MASKS | STARTLEDS_EXT_MASKS);
+	uint16_t dpadPressed = gamepad->state.dpad & GAMEPAD_MASK_DPAD;
+
+	this->ledsStart.display();
+	this->ledsCoin.display();
+	this->ledsMarquee.display();
+
+	if (!(buttonsPressed && this->ready))
+		return;
+
+	if ((buttonsPressed & COIN_BUTTON_MASKS) && dpadPressed)
+	{
+		if (this->debounce(&this->debounceMarqueeBrightness) || !this->ledsMarquee.isReady())
+			return;
+		if (dpadPressed & GAMEPAD_MASK_UP)
+			this->ledsMarquee.brightnessUp();
+		if (dpadPressed & GAMEPAD_MASK_DOWN)
+			this->ledsMarquee.brightnessDown();
+		return;
+	}
+
+	if (buttonsPressed == this->lastButtonsPressed)
+		return;
+	
+	this->lastButtonsPressed = buttonsPressed;
+
+	if ((buttonsPressed & STARTLEDS_EXT_MASKS))
+		if (this->externalStartPin != 0xFF)
+			(buttonsPressed & STARTLEDS_EXT_START_MASK) ?  gpio_put(this->externalStartPin, 1) : gpio_put(this->externalStartPin, 0);
+		if (this->externalCoinPin != 0xFF)
+			(buttonsPressed & STARTLEDS_EXT_COIN_MASK) ?  gpio_put(this->externalCoinPin, 1) : gpio_put(this->externalCoinPin, 0);			
+
+	if (!this->ledsStart.isReady() || !this->ledsCoin.isReady() || dpadPressed)
+		return;
+
+	if (buttonsPressed & COIN_BUTTON_MASKS)
+	{				
+		if (this->creditCount == 0)
+		{
+			this->ledsStart.setAnimation(STARTLEDS_BLINK_FAST_ALL);
+			this->ledsCoin.setAnimation(STARTLEDS_ALL_ON);
+		}
+		if (this->creditCount < 0xFF)
+			this->creditCount++;
+	}
+	else if (buttonsPressed & START_BUTTON_MASKS) 
+	{
+		if (this->creditCount > 0)
+			this->creditCount--;				
+		if (this->creditCount == 0)
+		{
+			this->ledsStart.setAnimation(STARTLEDS_ALL_OFF);
+			this->ledsCoin.setAnimation(STARTLEDS_FADE_ALL);
+		}
+		else
+		{
+			this->ledsStart.setAnimation(STARTLEDS_ALL_ON);
+		}
+	}	
+}
+
+bool StartLedsAddon::debounce(uint32_t * debounceTime)
+{
+	if (STARTLEDS_DEBOUNCE_MILLIS <= 0)
+		return true;
+				
+    uint32_t nowTime = getMillis();
+
+    if ((nowTime - *debounceTime) > STARTLEDS_DEBOUNCE_MILLIS)
+	{
+        *debounceTime = nowTime;
+		return false;
+    }
+    return true;
+}
