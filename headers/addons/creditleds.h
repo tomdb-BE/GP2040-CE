08a8b786e5f020fad525b11ae2784ab672cc0f1d
Tom De Backer <tomdebacker1@gmail.com>
2024-01-15 16:46:27 +0100

    Before merge

---
+++ headers/addons/creditleds.h
@@ -0,0 +1,205 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2022 Tom De Backer
+ */
+
+#ifndef _CREDITLEDS_H_
+#define _CREDITLEDS_H_
+
+#include "BoardConfig.h"
+#include <stdint.h>
+#include "AnimationStation.hpp"
+#include "gpaddon.h"
+#include "helper.h"
+#include "GamepadState.h"
+
+#ifndef CREDITLEDS_ENABLED
+#define CREDITLEDS_ENABLED 0
+#endif
+#ifndef CREDITLEDS_COUNT
+#define CREDITLEDS_COUNT 2
+#endif
+#ifndef CREDITLEDS_MAX_BRIGHTNESS
+#define CREDITLEDS_MAX_BRIGHTNESS 0xFF
+#endif
+#ifndef CREDITLEDS_PWM_MAXLEVEL
+#define CREDITLEDS_PWM_MAXLEVEL 0xFFFF
+#endif
+#ifndef CREDITLEDS_START1_BUTTON_MASK
+#define CREDITLEDS_START1_BUTTON_MASK GAMEPAD_MASK_S2
+#endif
+#ifndef CREDITLEDS_COIN1_BUTTON_MASK
+#define CREDITLEDS_COIN1_BUTTON_MASK GAMEPAD_MASK_S1
+#endif
+#ifndef CREDITLEDS_START2_BUTTON_MASK
+#define CREDITLEDS_START2_BUTTON_MASK GAMEPAD_MASK_R3
+#endif
+#ifndef CREDITLEDS_COIN2_BUTTON_MASK
+#define CREDITLEDS_COIN2_BUTTON_MASK GAMEPAD_MASK_L3
+#endif
+#ifndef CREDITLEDS_EXT_START_MASK
+#define CREDITLEDS_EXT_START_MASK CREDITLEDS_START2_BUTTON_MASK
+#endif
+#ifndef CREDITLEDS_EXT_COIN_MASK
+#define CREDITLEDS_EXT_COIN_MASK CREDITLEDS_COIN2_BUTTON_MASK
+#endif
+#ifndef CREDITLEDS_START_PIN1
+#define CREDITLEDS_START_PIN1 -1
+#endif
+#ifndef CREDITLEDS_COIN_PIN1
+#define CREDITLEDS_COIN_PIN1 -1
+#endif
+#ifndef CREDITLEDS_START_PIN2
+#define CREDITLEDS_START_PIN2 -1
+#endif
+#ifndef CREDITLEDS_COIN_PIN2
+#define CREDITLEDS_COIN_PIN2 -1
+#endif
+#ifndef CREDITLEDS_MARQUEE_PIN
+#define CREDITLEDS_MARQUEE_PIN -1
+#endif
+#ifndef CREDITLEDS_EXT_START_PIN
+#define CREDITLEDS_EXT_START_PIN -1
+#endif
+#ifndef CREDITLEDS_EXT_COIN_PIN
+#define CREDITLEDS_EXT_COIN_PIN -1
+#endif
+#ifndef CREDITLEDS_START_BRIGHTNESS
+#define CREDITLEDS_START_BRIGHTNESS 0xFF
+#endif
+#ifndef CREDITLEDS_COIN_BRIGHTNESS
+#define CREDITLEDS_COIN_BRIGHTNESS 0xFF
+#endif
+#ifndef CREDITLEDS_MARQUEE_BRIGHTNESS
+#define CREDITLEDS_MARQUEE_BRIGHTNESS 0xFF
+#endif
+#ifndef CREDITLEDS_BRIGHTNESS_STEP
+#define CREDITLEDS_BRIGHTNESS_STEP 5
+#endif
+#ifndef CREDITLEDS_DEBOUNCE_MILLIS
+#define CREDITLEDS_DEBOUNCE_MILLIS 50
+#endif
+
+static const uint8_t CREDITLEDS_STATE_LED1 = (1 << 0);
+static const uint8_t CREDITLEDS_STATE_LED2 = (1 << 1);
+static const uint8_t CREDITLEDS_STATE_LED3 = (1 << 2);
+static const uint8_t CREDITLEDS_STATE_LED4 = (1 << 3);
+static const uint8_t CREDITLEDS_STATE_ALL_ON  = 0xFF;
+static const uint8_t CREDITLEDS_STATE_ALL_OFF = 0;
+
+static const uint16_t CREDITLEDS_START_BUTTON_MASKS = CREDITLEDS_START1_BUTTON_MASK | CREDITLEDS_START2_BUTTON_MASK;
+static const uint16_t CREDITLEDS_COIN_BUTTON_MASKS = CREDITLEDS_COIN1_BUTTON_MASK | CREDITLEDS_COIN2_BUTTON_MASK;
+static const uint16_t CREDITLEDS_EXT_BUTTON_MASKS = CREDITLEDS_EXT_START_MASK | CREDITLEDS_EXT_COIN_MASK;
+
+typedef enum : uint8_t
+{
+	CREDITLEDS_ANIM_NONE = 255,
+	CREDITLEDS_ANIM_OFF = 0,
+	CREDITLEDS_ANIM_SOLID = 1,
+	CREDITLEDS_ANIM_BLINK = 2,
+	CREDITLEDS_ANIM_CYCLE = 3,
+	CREDITLEDS_ANIM_FADE = 4,
+} CreditLedsAnimationType;
+
+typedef enum : uint16_t
+{
+	CREDITLEDS_SPEED_OFF       = 0,
+	CREDITLEDS_SPEED_LUDICROUS = 20,
+	CREDITLEDS_SPEED_FASTER    = 100,
+	CREDITLEDS_SPEED_FAST      = 250,
+	CREDITLEDS_SPEED_NORMAL    = 500,
+	CREDITLEDS_SPEED_SLOW      = 1000,
+} CreditLedsAnimationSpeed;
+
+struct CreditLedsAnimation
+{
+	uint8_t currentState = CREDITLEDS_STATE_ALL_OFF;		
+	uint8_t stateMask = 0xFF;
+	uint8_t prevStateMask = 0xFF;
+	CreditLedsAnimationType currentType = CREDITLEDS_ANIM_NONE;
+	CreditLedsAnimationType previousType = CREDITLEDS_ANIM_NONE;
+	uint16_t speed = CREDITLEDS_SPEED_OFF;
+};
+
+static const CreditLedsAnimation CREDITLEDS_ALL_OFF {
+	currentState: CREDITLEDS_STATE_ALL_OFF,
+	stateMask: 0,
+	currentType: CREDITLEDS_ANIM_OFF
+};
+
+static const CreditLedsAnimation CREDITLEDS_ALL_ON {
+	currentState: CREDITLEDS_STATE_ALL_ON,
+	currentType: CREDITLEDS_ANIM_SOLID
+};
+
+static const CreditLedsAnimation CREDITLEDS_BLINK_FAST_ALL {
+	currentState: CREDITLEDS_STATE_ALL_OFF,
+	currentType: CREDITLEDS_ANIM_BLINK,
+	speed: CREDITLEDS_SPEED_FAST
+};
+
+static const CreditLedsAnimation CREDITLEDS_FADE_ALL {
+	currentState: CREDITLEDS_STATE_ALL_ON,
+	currentType: CREDITLEDS_ANIM_FADE,
+	speed: CREDITLEDS_SPEED_LUDICROUS
+};
+
+
+#define CreditLedsAddonName "CREDITLEDSADDON"
+
+class CreditLeds
+{
+public:		
+	void initAnimation() { this->animation.previousType = CREDITLEDS_ANIM_NONE; }
+	void setAnimation(CreditLedsAnimation newAnimation) { this->animation = newAnimation; this->nextAnimationTime = make_timeout_time_ms(0); }	
+	void setAnimationBrightness(uint8_t amount = CREDITLEDS_BRIGHTNESS_STEP) { this->handleBrightness(this->maxBrightness, amount); this->maxAnimationBrightness = this->brightness; }	
+	uint8_t getAnimationBrightness() {return this->maxAnimationBrightness; }
+	bool toggleState() {(this->turnedOff) ? this->turnOn() : this->turnOff(); return !this->turnedOff;}
+	bool isReady() {return this->ready; }
+	std::vector<uint> initialize(std::vector<uint> slices, uint8_t * pins, uint8_t mBrightness, CreditLedsAnimation initAnimation);
+	void display();	
+private:
+	void turnOn() {this->animation.stateMask = this->animation.prevStateMask; this->nextAnimationTime = make_timeout_time_ms(0); this->turnedOff = false; }
+	void turnOff() {this->animation.prevStateMask = this->animation.stateMask; this->animation.stateMask = 0; this->nextAnimationTime = make_timeout_time_ms(0); this->turnedOff = true; }
+	void handleBrightness(uint8_t mBrightness, uint8_t amount = CREDITLEDS_BRIGHTNESS_STEP);
+	inline void resetAnimation();
+	inline void animate();
+	const uint8_t maxBrightness = CREDITLEDS_MAX_BRIGHTNESS;
+	absolute_time_t nextAnimationTime = make_timeout_time_ms(0);
+	uint16_t ledLevels[CREDITLEDS_COUNT];
+	uint8_t ledPins[CREDITLEDS_COUNT];
+	uint8_t brightness;	
+	uint8_t maxAnimationBrightness;
+	CreditLedsAnimation animation;
+	bool fadeIn;
+	bool turnedOff;
+	bool ready;
+};
+
+class CreditLedsAddon : public GPAddon
+{
+public:
+	virtual bool available();
+	virtual void setup();
+	virtual void preprocess() {}
+	virtual void process();
+	virtual std::string name() { return CreditLedsAddonName; }	
+private:
+	bool debounce();
+	void saveBrightness();
+	void processBrightness(uint8_t dpadPressedMask);
+	void processExternalButtons(uint16_t buttonsPressedMask);
+	void processCredits(uint16_t buttonsPressedMask);
+	CreditLeds ledsStart;
+	CreditLeds ledsCoin;
+	CreditLeds ledsMarquee;
+	uint8_t creditCount = 0;
+	uint16_t lastButtonsPressed;
+	uint16_t lastDpadPressed;
+	uint32_t debounceBrightness;
+	uint8_t externalStartPin = 0xFF;
+	uint8_t externalCoinPin	= 0xFF;
+	bool ready;
+};
+
+#endif
---
+++ src/addons/creditleds.cpp
@@ -0,0 +1,290 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * SPDX-FileCopyrightText: Copyright (c) 2022 Tom De Backer
+ */
+
+// Pico Includes
+#include <vector>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "GamepadEnums.h"
+#include "storagemanager.h"
+
+// GP2040 Includes
+#include "addons/creditleds.h"
+#include "helper.h"
+
+
+inline void CreditLeds::resetAnimation()
+{	
+	this->animation.previousType = this->animation.currentType;	
+	this->brightness = this->maxAnimationBrightness;
+	this->fadeIn = false;
+	if (this->animation.currentType == CREDITLEDS_ANIM_FADE)
+		this->animation.speed = CREDITLEDS_SPEED_LUDICROUS + (this->maxBrightness - this->maxAnimationBrightness) / 2;
+	this->nextAnimationTime = make_timeout_time_ms(0);
+}
+
+void CreditLeds::handleBrightness(uint8_t mBrightness, uint8_t amount)
+{
+	uint8_t nBrightness = this->brightness;
+	if (this->fadeIn)	
+	{					
+		nBrightness += amount;	
+		if (nBrightness >= mBrightness || nBrightness < this->brightness) {
+			this->fadeIn = false;
+			nBrightness = mBrightness;
+		}		
+	}
+	else
+	{
+		nBrightness -= amount;
+		if (nBrightness > this->brightness) {
+			this->fadeIn = true;
+			nBrightness = 0;
+		}
+	}
+	this->brightness = nBrightness;
+}
+
+inline void CreditLeds::animate()
+{
+	uint8_t newState = this->animation.currentState;
+	uint8_t mask = this->animation.stateMask;
+	if (this->animation.currentType != this->animation.previousType)	
+		this->resetAnimation();			
+	else if (!time_reached(this->nextAnimationTime))	
+		return;			
+
+	switch (this->animation.currentType)
+	{
+		case CREDITLEDS_ANIM_OFF:
+			newState = CREDITLEDS_STATE_ALL_OFF | mask;
+			break;
+
+		case CREDITLEDS_ANIM_SOLID:
+			newState = CREDITLEDS_STATE_ALL_ON & mask;
+			break;	
+
+		case CREDITLEDS_ANIM_BLINK:
+			newState = ~newState & mask;
+			this->nextAnimationTime = make_timeout_time_ms(this->animation.speed);
+			break;
+
+		case CREDITLEDS_ANIM_FADE:
+			newState = newState & mask;
+			this->handleBrightness(this->maxAnimationBrightness);
+			this->nextAnimationTime = make_timeout_time_ms(this->animation.speed);
+			break;
+
+		default:
+			break;
+	}
+
+	this->animation.currentState = newState;
+
+	for (uint8_t i = 0; i < CREDITLEDS_COUNT; i++)
+		if (this->ledPins[i] != 0xFF)
+			this->ledLevels[i] = (newState & (1 << i)) ? this->brightness * this->brightness : 0;			
+}
+
+std::vector<uint> CreditLeds::initialize(std::vector<uint> slices, uint8_t * pins, uint8_t mBrightness, CreditLedsAnimation initAnimation) 
+{
+	if (pins[0] == 0xFF)
+		return slices;
+		
+	uint8_t newPin;
+	this->brightness = mBrightness;	
+	this->maxAnimationBrightness = mBrightness;
+	for (uint8_t i=0; i < CREDITLEDS_COUNT; i++)
+	{
+		newPin = pins[i];
+		this->ledPins[i] = newPin;
+		if (newPin != 0xFF)
+		{
+			gpio_set_function(newPin, GPIO_FUNC_PWM);
+			uint sliceNum = pwm_gpio_to_slice_num(newPin);
+			uint channelNum = pwm_gpio_to_channel(newPin);
+			slices.push_back(sliceNum);
+			pwm_set_chan_level(sliceNum, channelNum, CREDITLEDS_PWM_MAXLEVEL);			
+		}
+	}
+	this->animation = initAnimation;
+	this->animate();
+	this->ready = true;
+	return slices;
+}
+
+void CreditLeds::display()
+{
+	if (! this->ready)
+		return;
+
+	this->animate();
+	for (int i = 0; i < CREDITLEDS_COUNT; i++)
+		if (this->ledPins[i] != 0xFF)
+			pwm_set_gpio_level(this->ledPins[i], this->ledLevels[i]);
+}
+
+// ADDON
+
+bool CreditLedsAddon::debounce()
+{
+	if (CREDITLEDS_DEBOUNCE_MILLIS <= 0)
+		return false;
+				
+    uint32_t nowTime = getMillis();
+
+    if ((nowTime - this->debounceBrightness) > CREDITLEDS_DEBOUNCE_MILLIS)
+	{
+        this->debounceBrightness = nowTime;
+		return false;
+    }
+    return true;
+}
+
+void CreditLedsAddon::saveBrightness()
+{
+	AddonOptions options = Storage::getInstance().getAddonOptions();
+	options.creditLedsStartBrightness = this->ledsStart.getAnimationBrightness();
+	options.creditLedsCoinBrightness =  this->ledsCoin.getAnimationBrightness();
+	options.creditLedsMarqueeBrightness = this->ledsMarquee.getAnimationBrightness();
+	Storage::getInstance().setAddonOptions(options);
+}
+
+bool CreditLedsAddon::available() {
+	AddonOptions options = Storage::getInstance().getAddonOptions();	
+	return options.creditLedsAddonEnabled;
+}
+
+void CreditLedsAddon::setup() {	
+	AddonOptions options = Storage::getInstance().getAddonOptions();
+	uint8_t startPins[] = {options.creditLedsStartPin1, options.creditLedsStartPin2};
+	uint8_t coinPins[] = {options.creditLedsCoinPin1, options.creditLedsCoinPin2};
+	uint8_t marqueePins[] = {options.creditLedsMarqueePin, 0xFF};
+	uint8_t extStartPin = options.creditLedsExtStartPin;
+	uint8_t extCoinPin = options.creditLedsExtCoinPin;
+	uint8_t startBrightness = options.creditLedsStartBrightness;	
+	uint8_t coinBrightness = options.creditLedsCoinBrightness;	
+	uint8_t marqueeBrightness = options.creditLedsMarqueeBrightness;
+
+	pwm_config config = pwm_get_default_config();
+	pwm_config_set_clkdiv(&config, 4.f);
+
+	std::vector<uint> sliceNums;
+
+	sliceNums = this->ledsStart.initialize(sliceNums, startPins, startBrightness, CREDITLEDS_ALL_OFF); 
+	sliceNums = this->ledsCoin.initialize(sliceNums, coinPins, coinBrightness, CREDITLEDS_ALL_ON);
+	sliceNums = this->ledsMarquee.initialize(sliceNums, marqueePins, marqueeBrightness, CREDITLEDS_ALL_ON);
+
+	for (auto sliceNum : sliceNums)
+		pwm_set_enabled(sliceNum, true);
+	
+	if (extStartPin != 0xFF)
+	{
+		gpio_init(extStartPin);
+    	gpio_set_dir(extStartPin, GPIO_OUT);
+		gpio_put(extStartPin, 1);
+		this->externalStartPin = extStartPin;
+	}
+
+	if (extCoinPin != 0xFF)
+	{
+		gpio_init(extCoinPin);
+    	gpio_set_dir(extCoinPin, GPIO_OUT);
+		gpio_put(extCoinPin, 1);
+		this->externalCoinPin = extCoinPin;
+	}
+	this->ready = true;
+}
+
+void CreditLedsAddon::processBrightness(uint8_t dpadPressedMask)
+{	
+	if (dpadPressedMask	== GAMEPAD_MASK_DOWN && this->lastDpadPressed != GAMEPAD_MASK_DOWN) {		
+		this->ledsMarquee.toggleState();
+		this->ledsCoin.toggleState();
+		if (this->ledsStart.toggleState())
+			this->saveBrightness();
+		return;
+	}
+
+	if (this->debounce())
+		return;
+	
+	switch (dpadPressedMask)
+	{
+		case GAMEPAD_MASK_UP:    this->ledsMarquee.setAnimationBrightness(); break;			
+		case GAMEPAD_MASK_LEFT:  this->ledsCoin.setAnimationBrightness();    break;			
+		case GAMEPAD_MASK_RIGHT: this->ledsStart.setAnimationBrightness();   break;
+	}	
+}
+
+void CreditLedsAddon::processExternalButtons(uint16_t buttonsPressedMask)
+{
+	if (this->externalStartPin != 0xFF)
+		(buttonsPressedMask & CREDITLEDS_EXT_START_MASK) ?  gpio_put(this->externalStartPin, 0) : gpio_put(this->externalStartPin, 1);
+	if (this->externalCoinPin != 0xFF)
+		(buttonsPressedMask & CREDITLEDS_EXT_COIN_MASK) ?  gpio_put(this->externalCoinPin, 0) : gpio_put(this->externalCoinPin, 1);
+}
+
+void CreditLedsAddon::processCredits(uint16_t buttonsPressedMask)
+{	
+	if (buttonsPressedMask & CREDITLEDS_COIN_BUTTON_MASKS)
+	{				
+		if (this->creditCount == 0)
+		{
+			this->ledsStart.setAnimation(CREDITLEDS_BLINK_FAST_ALL);
+			this->ledsCoin.setAnimation(CREDITLEDS_ALL_ON);
+		}
+		if (this->creditCount < 0xFF)
+			this->creditCount++;
+		return;
+	}
+	if (buttonsPressedMask & CREDITLEDS_START_BUTTON_MASKS) 
+	{
+		if (this->creditCount == 0) {
+			this->ledsCoin.setAnimation(CREDITLEDS_FADE_ALL);
+			return;
+		}
+		this->creditCount--;
+		if (this->creditCount == 0)		
+			this->ledsStart.setAnimation(CREDITLEDS_ALL_OFF);
+		else		
+			this->ledsStart.setAnimation(CREDITLEDS_ALL_ON);
+		return;
+	}
+}
+
+void CreditLedsAddon::process()
+{
+    Gamepad * gamepad = Storage::getInstance().GetProcessedGamepad();	
+    
+	uint16_t buttonsPressed = gamepad->state.buttons & (CREDITLEDS_START_BUTTON_MASKS | CREDITLEDS_COIN_BUTTON_MASKS | CREDITLEDS_EXT_BUTTON_MASKS);
+	uint8_t dpadPressed = gamepad->state.dpad & GAMEPAD_MASK_DPAD;
+
+	this->ledsStart.display();
+	this->ledsCoin.display();
+	this->ledsMarquee.display();
+
+	if (!this->ready)
+		return;	
+
+	if (dpadPressed)
+	{
+		if (buttonsPressed & CREDITLEDS_COIN_BUTTON_MASKS)
+			this->processBrightness(dpadPressed);
+		this->lastDpadPressed = dpadPressed;
+		this->lastButtonsPressed = buttonsPressed;	
+		return;
+	}
+	this->lastDpadPressed = 0;
+
+	if (buttonsPressed == this->lastButtonsPressed)
+		return;
+	this->lastButtonsPressed = buttonsPressed;
+
+	this->processExternalButtons(buttonsPressed);
+	if (buttonsPressed)
+		this->processCredits(buttonsPressed);
+
+}
